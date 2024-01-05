ae16aef5eafc408ac0f5237b3b70606d26ba17d8
MaxPayne8 <maxpayne0852.com>
2024-01-05 21:17:17 +0530

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import heroImg from "../images/heroImg.png";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800  "
+    >
+      <div
+        className=" mx-auto flex flex-col
+           justify-center items-center  h-screen px-10 text-slate-300 md:flex-row max-w-screen-lg "
+      >
+        <div className=" text-left w-4/5">
+          <h2 className=" text-4xl font-bold">I'm a Front end Developer</h2>
+          <p className="text-slate-400">
+            I'm passionate about creating great user experiences and have a
+            strong understanding of usability and accessibility standards.
+            Currently, I love to work on web applications using technologies
+            like React, Redux and Tailwind.
+          </p>
+          <div>
+            <button className="flex items-center w-fit px-6 py-3 my-6 md:my-2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg group">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300 ">
+                <MdOutlineKeyboardArrowRight size={25} />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img
+            className="w-56 h-64  rounded-xl hover:animate-spin cursor-pointer border-2 border-white"
+            src={heroImg}
+            alt="hero"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    { id: 1, link: "Home" },
+    { id: 2, link: "About" },
+    { id: 3, link: "Portfolio" },
+    { id: 4, link: "Projects" },
+    { id: 5, link: "Contact" },
+  ];
+  return (
+    <div className="flex justify-between h-20 bg-black items-center text-slate-200 fixed w-full px-4">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Zatin</h1>
+      </div>
+      <ul className="hidden md:flex  ">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="py-2 px-8 hover:cursor-pointer font-medium  text-slate-500 hover:scale-105 hover:text-slate-200 duration-150"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="hover:cursor-pointer block md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+      {nav === true && (
+        <ul
+          className="flex flex-col justify-center items-center
+          absolute top-20 left-0  w-full h-screen bg-gradient-to-b from-black to-gray-500 ☐ text-gray-200   "
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="p-2 hover:cursor-pointer font-medium text-4xl py-6 hover:scale-105 text-slate-200 duration-150"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
